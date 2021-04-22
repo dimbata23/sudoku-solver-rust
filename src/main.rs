0d99388257ec336de16d8a6865a5ce95045f80f5
dimbata23 <adimitrov23@gmail.com>
2021-04-22 22:56:37 +0300

    follow-up commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod sudoku;
+
+use std::io;
+use sudoku::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+
+    let mut file    = File::open( "test.txt" )?;
+    let     sud     = Sudoku::from_file( &mut file )?;
+    
+    sud.print();
+
+    Ok(())
 }
