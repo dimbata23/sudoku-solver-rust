d320f98d0587586e619c0bc375e08733f0c35290
dimbata23 <adimitrov23@gmail.com>
2021-04-30 15:03:37 +0300

    added timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod sudoku;
 
+use std::time::Instant;
 use std::io;
 use sudoku::*;
 use std::fs::File;
@@ -9,8 +10,15 @@ fn main() -> io::Result<()> {
     let mut file    = File::open( "test.txt" )?;
     let mut sud     = Sudoku::from_file( &mut file )?;
     
-    sud.solve();
-    sud.print();
+    let start = Instant::now();
+    if sud.solve() {
+        let elapsed = start.elapsed().as_millis();
+        println!( "Solution found in {}ms.", elapsed );
+        sud.print();
+    }
+    else {
+        println!( "Couldn't solve the Sudoku!" );
+    }
 
     Ok(())
 }
